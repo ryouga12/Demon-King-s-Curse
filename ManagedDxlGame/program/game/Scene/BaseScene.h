0ab36475ca7b39fb5d16666259925d02eaed7d9a
ryouga12 <ryouga.20011108@gmail.com>
2024-02-09 18:18:39 +0900

    [修正]BaseScnenの修正

--- ManagedDxlGame/program/game/Scene/BaseScene.h
+++ ManagedDxlGame/program/game/Scene/BaseScene.h
@@ -1,9 +1,30 @@
 #pragma once
 //シーンの基底クラス
+#include "../Item/Inventory.h"
+
 
 
 class BaseScene {
 public:
     virtual void Update(float delta_time) {};
     virtual void Draw() = 0;
+   
+
+//------------------------------------------------------------------------------------------------------------------------
+//他のシーンなどでも使うため共通メソッドにする
+   Shared<Inventory>inventory = nullptr;
+
+   // 共通処理やインベントリの更新
+   void UpdateCommon(float delta_time) {
+       inventory->Update(delta_time);
+   }
+   void DrawCommon() {
+       inventory->draw();
+   }
+   void SetInventory(Shared<Inventory>inventory_) {
+       inventory = inventory_;
+   }
+
 };
+
+
